1a0561a278bb86e9e323cf8de97fe9f598f300e0
Yuta-Tsutsumi <watagasi.789@gmail.com>
2023-01-17 00:05:31 +0900

    tsxファイルの新規作成

---
+++ src/views/components/movieItem/MovieItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieItem: React.FC = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default MovieItem;
---
+++ src/views/components/movieList/MovieList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieList: React.FC = () => {
+  return <div>MovieList</div>;
+};
+
+export default MovieList;
