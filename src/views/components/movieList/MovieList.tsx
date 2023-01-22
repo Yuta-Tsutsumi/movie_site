5506e3dba7ce25313d5ac7888ec96e94009dd32d
Yuta-Tsutsumi <watagasi.789@gmail.com>
2023-01-22 09:18:31 +0900

    MovieListの実装

--- src/views/components/movieList/MovieList.tsx
+++ src/views/components/movieList/MovieList.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { MovieListType } from "../../types/movieTypes";
+import MovieItem from "../movieItem/MovieItem";
+import styles from "./MovieList.module.scss";
 
-const MovieList: React.FC = () => {
-  return <div>MovieList</div>;
+// ここでMovieListコンポーネントにわたすべきPropsの型を定義している
+type Props = { movieList: MovieListType };
+
+const MovieList: React.FC<Props> = (props) => {
+  // 今からこのデータを1つ1つMolvieItemに渡していく
+  const movieList = props.movieList;
+
+  // movieList配列（20個のデータ）の情報を1つ1つ展開したい
+  return (
+    <div className={styles.root}>
+      {movieList.map((movie) => {
+        return <MovieItem item={movie} key={movie.id} />;
+      })}
+    </div>
+  );
 };
 
 export default MovieList;
