ccd6f8b06f6eb9c35bd9be9d2a2314780bd3ab27
Yuta-Tsutsumi <watagasi.789@gmail.com>
2023-01-22 09:18:03 +0900

    MovieItemの実装

--- src/views/components/movieItem/MovieItem.tsx
+++ src/views/components/movieItem/MovieItem.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { MovieItemType } from "../../types/movieTypes";
+import styles from "./MovieItem.module.scss";
 
-const MovieItem: React.FC = () => {
+type Props = {
+  // 映画情報の型の定義（id、映画のポスター画像、映画タイトル、映画の概要説明）
+  item: MovieItemType;
+};
+
+const MovieItem: React.FC<Props> = ({ item }) => {
   return (
-    <div>
-      <div></div>
+    <div className={styles.root}>
+      <div className={styles.posterBox}>
+        <div className={styles.posterItems}>
+          <div className={styles.title}>{item.title}</div>
+          <div className={styles.poster_path}>
+            <img
+              className={styles.posterImage}
+              src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+              alt="movie poster"
+            />
+          </div>
+          <div className={styles.overview}>{item.overview}</div>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default MovieItem;
